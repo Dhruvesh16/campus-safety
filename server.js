8816cece34e3053d81fac2877191e0d861501d08
dhruvesh16 <dhruvesh3466@protonmail.com>
2024-12-11 08:50:30 +0530

    Nuke depreciated methods

--- server.js
+++ server.js
@@ -12,14 +12,13 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/campus-safety', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
+mongoose.connect('mongodb://127.0.0.1:27017/campus-safety')
+  .then(() => {
     console.log('Connected to MongoDB');
-}).catch(err => {
-    console.error('MongoDB connection error:', err);
-});
+  })
+  .catch((error) => {
+    console.error('MongoDB connection error:', error);
+  });
 
 // Incident Model
 const incidentSchema = new mongoose.Schema({
